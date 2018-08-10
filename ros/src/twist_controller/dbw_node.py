bc5954ebcfdfe5511ad018d484a51f3388e0dd6c
iansebas <iansebas@umich.edu>
2018-08-09 17:06:41 -0700

    ...

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -96,7 +96,7 @@ class DBWNode(object):
         self.linear_vel = msg.twist.linear.x
         self.angular_vel = msg.twist.angular.z
 
-    def velocity_cb(self):
+    def velocity_cb(self, msg):
         self.current_vel = msg.twist.linear.x
 
     def publish(self, throttle, brake, steer):
